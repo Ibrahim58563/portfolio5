59709feb6ff27e413577d3315e5f4c3ab9b1e996
ibrahimmansour <ibrahimmansour@gmail.com>
2022-08-12 22:53:57 +0200

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Instagram, Twitter, Facebook, LinkedIn } from "@mui/icons-material";
+import "../styles/Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="socialMedia">
+        <Instagram />
+        <Twitter />
+        <Facebook />
+        <LinkedIn />
+      </div>
+      <p> &copy; 2022 pedrotech.com</p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/pages/Home.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { GitHub, Email, LinkedIn } from "@mui/icons-material";
+import "../styles/Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="about">
+        <h2> Hi, My Name is Ibrahmin Mansour</h2>
+        <div className="prompt">
+          <p>A software developer with a passion for learning and creating.</p>
+          <LinkedIn />
+          <Email />
+          <GitHub />
+        </div>
+      </div>
+      <div className="skills">
+        <h1> Skills</h1>
+        <ol className="list">
+          <li className="item">
+            <h2> Front-End</h2>
+            <span>
+              ReactJS, Angular, Redux, HTML, CSS, React Native, Flutter, NPM,
+              Ionic, BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents
+            </span>
+          </li>
+          <li className="item">
+            <h2>Back-End</h2>
+            <span>NodeJS , ExpressJS MySQL, MongoDB, AWS </span>
+          </li>
+          <li className="item">
+            <h2>Languages</h2>
+            <span>JavaScript, Java, C#, C, C++, TypeScript</span>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
